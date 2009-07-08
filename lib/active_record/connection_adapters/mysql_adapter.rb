2678b80ef480eff7ffe24790da97610833d20efa
Nicholas Zalabak <nick@controlaltcreate.com>
2009-07-08 12:09:53 -0500

    techwhizbang (Nick Zalabak): commit fix for mysql tables with non standard primary keys such that the schema dump is correct

--- lib/active_record/connection_adapters/mysql_adapter.rb
+++ lib/active_record/connection_adapters/mysql_adapter.rb
@@ -11,3 +11,23 @@ rescue LoadError
   # trust that the mysql jar is already present
 end
 require 'active_record/connection_adapters/jdbc_adapter'
+
+module ActiveRecord
+  module ConnectionAdapters
+    class JdbcAdapter < AbstractAdapter
+
+      # Returns a table's primary key and belonging sequence.
+      def pk_and_sequence_for(table)
+        keys = nil
+        java_jdbc_result_set = @connection.connection.get_meta_data.get_primary_keys(nil, nil, table)
+        has_first = java_jdbc_result_set.first
+        if has_first
+          java_jdbc_result_set.first
+          keys = [java_jdbc_result_set.getString("COLUMN_NAME"), nil]
+        end
+
+        keys.blank? ? nil : keys
+      end
+    end
+  end
+end
